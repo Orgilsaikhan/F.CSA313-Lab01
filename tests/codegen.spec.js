c304695c6fc308c5fd120ca0174de9a54538f4e9
Hugo <orgilsaikhan.g07@gmail.com>
2026-09-09 17:18:14 +0800

    Codegen test

---
+++ tests/codegen.spec.js
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('Codegen test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await expect(page.locator('[data-test="username"]')).toBeVisible();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standart_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="item-4-title-link"]').click();
+  await page.locator('[data-test="item-sauce-labs-backpack-img"]').click();
+  await page.locator('[data-test="add-to-cart"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.getByText('Continue ShoppingCheckout').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('Orgilsaikhan');
+  await page.locator('[data-test="lastName"]').click();
+  await page.locator('[data-test="lastName"]').fill('Ganbadral');
+  await page.locator('[data-test="postalCode"]').click();
+  await page.locator('[data-test="postalCode"]').fill('16010');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="remove-sauce-labs-bike-light"]').click();
+  await page.locator('[data-test="continue-shopping"]').click();
+});
